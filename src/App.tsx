e99b58107febe4f4cdcceeadb4227efb69dc5ff3
ritagujar <ritagujar14@gmail.com>
2023-08-30 00:03:13 +0530

    Displayed tasks in the UI when the user clicks the add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AddToDo from "./components/AddToDo";
+import Todos from "./components/Todos";
 
 const App = () => {
   return (
     <main>
       <h1>TODO REACT + TYPESCRIPT</h1>
       <AddToDo />
+      <Todos />
     </main>
   );
 };
