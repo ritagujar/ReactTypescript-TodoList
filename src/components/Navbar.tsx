d6f0cf842f0e042ac31f4c058854e013d45b57ff
ritagujar <ritagujar14@gmail.com>
2023-08-30 01:37:12 +0530

    Added Hover effects for active and completed tasks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Navbar = () => {
+  const [searchParams] = useSearchParams();
+  let todosData = searchParams.get("todos");
+
   return (
     <nav>
-      <Link to="/">All</Link>
-      <Link to="/?todos=active">Active</Link>
-      <Link to="/?todos=completed">Completed</Link>
+      <Link to="/" className={todosData === null ? "active" : ""}>
+        All
+      </Link>
+      <Link
+        to="/?todos=active"
+        className={todosData === "active" ? "active" : ""}
+      >
+        Active
+      </Link>
+      <Link
+        to="/?todos=completed"
+        className={todosData === "completed" ? "active" : ""}
+      >
+        Completed
+      </Link>
     </nav>
   );
 };
