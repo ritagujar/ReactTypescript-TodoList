7beb43b5d9db5bc6120ddc45f2cf4d4872de32fd
ritagujar <ritagujar14@gmail.com>
2023-08-30 01:30:32 +0530

    Added CSS Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AddToDo from "./components/AddToDo";
 import Navbar from "./components/Navbar";
 import Todos from "./components/Todos";
+import "./App.css";
 
 const App = () => {
   return (
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -19,7 +19,7 @@ const Todos = () => {
   }
 
   return (
-    <ul>
+    <ul className="main-task">
       {filterData.map((todo) => {
         return (
           <li key={todo.id}>
